2de1d6f9ae742340d1d866e6f7aeb651faf2e90d
pazzazzo <54208876+pazzazzo@users.noreply.github.com>
2024-06-01 18:11:34 +0200

    Add USB listener

--- dashboard/src/script.js
+++ dashboard/src/script.js
@@ -9,8 +9,8 @@ const { Chart } = require("chart.js/auto")
 const mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
 const Buggy = require('./classes/Buggy');
 const Page = require('./classes/Page');
-
-;
+const usb = require("usb")
+const readline = require('readline');
 
 const versions = process.versions
 const infoBar = new InfoBar()
@@ -18,7 +18,7 @@ const buggy = new Buggy()
 const page = new Page()
 mediaLoader.init()
 
-page.view.change("data-graph")
+// page.view.change("data-graph")
 
 mediaLoader.on("ready", (t) => {
     page.change("main")
@@ -50,6 +50,58 @@ mediaLoader.on("ready", (t) => {
 
 console.log(`Electron: v${versions.electron}`);
 console.log(`Chrome: v${versions.chrome}`);
+
+function getUSBData(vendorIdDec, productIdDec) {
+    return new Promise((r, e) => {
+        const vendorIdHex = vendorIdDec.toString(16).padStart(4, '0');
+        const productIdHex = productIdDec.toString(16).padStart(4, '0');
+        const rl = readline.createInterface({
+            input: fs.createReadStream(path.join(__dirname, "usb.ids")),
+            output: process.stdout,
+            terminal: false
+        });
+
+        let currentVendor = null;
+
+        rl.on('line', (line) => {
+            const vendorMatch = line.match(/^([0-9a-fA-F]{4})\s+(.*)$/);
+            if (vendorMatch) {
+                const [_, id, name] = vendorMatch;
+                if (id.toLowerCase() === vendorIdHex.toLowerCase()) {
+                    currentVendor = name;
+                } else {
+                    currentVendor = null;
+                }
+            }
+
+            const productMatch = line.match(/^\t([0-9a-fA-F]{4})\s+(.*)$/);
+            if (currentVendor && productMatch) {
+                const [_, id, name] = productMatch;
+                if (id.toLowerCase() === productIdHex.toLowerCase()) {
+                    r({vendor: currentVendor, product: name, success: true})
+                    rl.close();
+                }
+            }
+        });
+
+        rl.on('close', () => {
+            r({success: false})
+        });
+
+    })
+}
+
+usb.usb.on("attach", (device) => {
+    getUSBData(device.deviceDescriptor.idVendor, device.deviceDescriptor.idProduct).then((r) => {
+        console.log(r);
+    })
+})
+usb.usb.on("detach", (device) => {
+    getUSBData(device.deviceDescriptor.idVendor, device.deviceDescriptor.idProduct).then((r) => {
+        console.log(r);
+    })
+})
+
 
 buggy.rearCam.getCams().then(v => {
     buggy.rearCam.setCam(v[0].id)
