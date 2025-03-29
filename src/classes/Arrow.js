1926ade42996215fa8d397f33087fadf25b0a0ab
pazzazzo <54208876+pazzazzo@users.noreply.github.com>
2025-03-29 23:29:07 +0100

    Update Arrow.js

--- src/classes/Arrow.js
+++ src/classes/Arrow.js
@@ -5,6 +5,7 @@ const MapboxObject = require("./MapboxObject");
 class Arrow extends MapboxObject {
     constructor(mediaLoader = new MediaLoader()) {
         super(mediaLoader, path.join(__dirname, "..", "media/images/arrow.png"))
+        console.log("✅ Arrow class invoked");
         this.update(mediaLoader.position.coords.array, mediaLoader.position.coords.heading)
         mediaLoader.position.coords.on("update", () => {
             this.update(mediaLoader.position.coords.array, mediaLoader.position.coords.heading)
