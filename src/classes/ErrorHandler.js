97da6c9ba2a40ce7ea9b3f35579b241a3460f7ec
pazzazzo <54208876+pazzazzo@users.noreply.github.com>
2025-04-14 11:39:34 +0200

    Update ErrorHandler.js

--- src/classes/ErrorHandler.js
+++ src/classes/ErrorHandler.js
@@ -1,15 +1,18 @@
 class ErrorHandler {
     constructor() {
         console.log("✅ ErrorHandler class invoked");
+        this.errorScreen = document.getElementById("error-screen")
         window.onerror = (...e) => {
-            const errorScreen = document.getElementById("error-screen")
-            let msg = `<div class="error-popup"><div class="error-title">${e[0]}</div><div class="error-file">${e[1]}:${e[2]}:${e[3]}</div><div class="error-details">${e[4].stack.replace(new RegExp(__dirname + "/", "g"), "")}</div></div>`
-            errorScreen.innerHTML += msg
-            errorScreen.classList.add("active")
+            let msg = `<div class="error-popup"><div class="error-title">${e[0]}</div><div class="error-file">${e[1]}:${e[2]}:${e[3]} (on ${mediaLoader?.status || "unknown"} phase)</div><div class="error-details">${e[4].stack.replace(new RegExp(__dirname + "/", "g"), "")}</div></div>`
+            this.errorScreen.innerHTML += msg
+            this.errorScreen.classList.add("active")
             if (mediaLoader && mediaLoader.playSound) {
                 mediaLoader.playSound("error")
             }
         }
+        this.errorScreen.addEventListener("click", () => {
+            this.errorScreen.classList.remove("active")
+        })
     }
 }
 
