f785ce28c05d5caebc68f317703591f5243a8ad2
pazzazzo <54208876+pazzazzo@users.noreply.github.com>
2025-04-27 22:21:14 +0200

    Fix install.sh

--- install.sh
+++ install.sh
@@ -3,9 +3,9 @@
 FORCE=0
 for arg in "$@"; do
   case "$arg" in
-    --force|-f)
-      FORCE=1
-      ;;
+  --force | -f)
+    FORCE=1
+    ;;
   esac
 done
 
@@ -27,28 +27,35 @@ sudo apt-get install -y bluetooth bluez libbluetooth-dev libudev-dev curl ofono
 echo "✅  Packages installés"
 
 # Vérifier si Node.js est installé
-if ! command -v node &> /dev/null
-then
-    echo "⚠️ Node.js n'est pas installé. Installation de Node.js..."
-    # installs nvm (Node Version Manager)
-    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
-    # download and install Node.js (you may need to restart the terminal)
-    nvm install 20
-    # verifies the right Node.js version is in the environment
-    node -v # should print `v20.16.0`
-    # verifies the right npm version is in the environment
-    npm -v # should print `10.8.1`
+if ! command -v node &>/dev/null; then
+  echo "⚠️ Node.js n'est pas installé. Installation de Node.js..."
+
+  set -euo pipefail
+  export NVM_DIR="$HOME/.nvm"
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+
+  if [ -s "$NVM_DIR/nvm.sh" ]; then
+    . "$NVM_DIR/nvm.sh"
+  else
+    echo "❌ nvm.sh introuvable dans $NVM_DIR"
+    exit 1
+  fi
+  # download and install Node.js (you may need to restart the terminal)
+  nvm install 20
+  # verifies the right Node.js version is in the environment
+  node -v # should print `v20.16.0`
+  # verifies the right npm version is in the environment
+  npm -v # should print `10.8.1`
 else
-    echo "✅ Node.js est installé."
+  echo "✅ Node.js est installé."
 fi
 
 # Vérifier si npm est installé
-if ! command -v npm &> /dev/null
-then
-    echo "❌ npm n'est pas installé. Une erreur est survenue"
-    exit
+if ! command -v npm &>/dev/null; then
+  echo "❌ npm n'est pas installé. Une erreur est survenue"
+  exit
 else
-    echo "✅ npm est installé."
+  echo "✅ npm est installé."
 fi
 
 # Installer les dépendances du projet Node.js
