950157c1e31bd54a2b46df00d5b6d6902ab3b70b
pazzazzo <54208876+pazzazzo@users.noreply.github.com>
2025-06-04 00:58:26 +0200

    Update Settings.js

--- src/apps/settings/Settings.js
+++ src/apps/settings/Settings.js
@@ -1,4 +1,3 @@
-const Chart = require("../../classes/Chart.js");
 const MediaLoader = require("../../classes/MediaLoader");
 const fs = require("fs");
 const Navbar = require("./Navbar.js");
@@ -8,10 +7,12 @@ const Alerts = require("./Alerts.js");
 const Camera = require("./Camera.js");
 const SIM = require("./SIM.js");
 const System = require("./System.js");
+const EventEmitter = require("events");
 
-class Settings {
+class Settings extends EventEmitter {
     constructor(mediaLoader = new MediaLoader()) {
         console.log("✅ SettingsApp invoked");
+        super()
         this.mediaLoader = mediaLoader;
     }
     init() {
@@ -87,113 +88,6 @@ class Settings {
             document.getElementById("settings-list-sim"),
             document.getElementById("settings-list-system"),
         ]
-        let camerasSelect = document.getElementsByClassName("settings-cams-selector")
-        let camerasPreview = document.getElementsByClassName("settings-cams-preview")
-        this.controls = {
-            "camerasSelecors": {
-                "dashcam": camerasSelect[0],
-                "front": camerasSelect[1],
-                "back": camerasSelect[2],
-            }
-        }
-        this.simChart = new Chart(document.getElementById("settings-sim-use-chart").getContext("2d"), {
-            unit: " MB",
-        })
-        this.simChart.addStat(this.mediaLoader.stats.dataUse, {
-            type: "getDatasetsByDatesNumber", args: [new Date(), -13], params: {
-                label: "Utilisation",
-                rounded: true,
-                fill: true
-            },
-            convertCallback: ((data) => Math.round(data / 1024 / 1024)),
-        })
-        const typeData = [
-            2.00,   // audio-ak.spotifycdn.com: 2 096 684 / 1 048 576 ≈ 2.00 Mo
-            0.68,   // web-sdk-assets.spotifycdn.com: 710 420 / 1 048 576 ≈ 0.68 Mo
-            0.30,   // i.scdn.co: 315 065 / 1 048 576 ≈ 0.30 Mo
-            0.30,   // api.mapbox.com: 312 446 / 1 048 576 ≈ 0.30 Mo
-            0.23,   // www.waze.com: 242 388 / 1 048 576 ≈ 0.23 Mo
-            0.03,   // api.wit.ai: 36 560 / 1 048 576 ≈ 0.03 Mo
-            0.03,   // cpapi.spotify.com: 35 212 / 1 048 576 ≈ 0.03 Mo
-            0.03,   // places.googleapis.com: 31 144 / 1 048 576 ≈ 0.03 Mo
-            0.03,   // api.spotify.com: 28 208 / 1 048 576 ≈ 0.03 Mo
-            0.02,   // gew1-dealer.spotify.com: 25 354 / 1 048 576 ≈ 0.02 Mo
-            0.01,   // seektables.scdn.co: 10 149 / 1 048 576 ≈ 0.01 Mo
-            0.01,   // events.mapbox.com: 8 894 / 1 048 576 ≈ 0.01 Mo
-            0.01,   // maps.googleapis.com: 8 389 / 1 048 576 ≈ 0.01 Mo
-            0.01    // apresolve.spotify.com: 7 063 / 1 048 576 ≈ 0.01 Mo
-        ]
-        const typeLabels = [
-            "audio-ak.spotifycdn.com",
-            "web-sdk-assets.spotifycdn.com",
-            "i.scdn.co",
-            "api.mapbox.com",
-            "www.waze.com",
-            "api.wit.ai",
-            "cpapi.spotify.com",
-            "places.googleapis.com",
-            "api.spotify.com",
-            "gew1-dealer.spotify.com",
-            "seektables.scdn.co",
-            "events.mapbox.com",
-            "maps.googleapis.com",
-            "apresolve.spotify.com"
-        ]
-        this.typeChart = new Chart(document.getElementById("settings-sim-type-chart").getContext("2d"), {
-            x: typeLabels,
-            y: [
-                {
-                    data: typeData,
-                    label: "Utilisation",
-                    rounded: true,
-                    fill: true
-                }
-            ],
-            unit: " MB",
-            type: "bar",
-            horizontal: true,
-            height: `${2 * typeLabels.length}rem`
-        })
-
-        //Récupérer toutes les caméras du navigateur et les ajouter à la liste
-        navigator.mediaDevices.enumerateDevices().then((devices) => {
-            devices.forEach((device) => {
-                if (device.kind === "videoinput") {
-                    for (const deviceType in this.controls.camerasSelecors) {
-                        let option = document.createElement("option");
-                        option.value = device.deviceId;
-                        option.innerHTML = device.label;
-                        if (device.deviceId === this.mediaLoader.settings.data.camera.device[deviceType]) {
-                            option.selected = true;
-                        }
-                        this.controls.camerasSelecors[deviceType].appendChild(option);
-                    }
-                }
-            })
-        })
-
-        for (let i = 0; i < camerasSelect.length; i++) {
-            const element = camerasSelect[i];
-            element.addEventListener("change", () => {
-                let camId = element.value;
-                if (camId == "null") {
-                    return camerasPreview[i].srcObject = null;
-                }
-
-                navigator.mediaDevices.getUserMedia({
-                    video: {
-                        deviceId: { exact: camId }
-                    }
-                }).then((stream) => {
-                    let video = camerasPreview[i];
-                    video.srcObject = stream;
-                    video.play();
-                }).catch((err) => {
-                    console.error(err);
-                })
-            })
-        }
-
 
         this.nav.forEach((el, index) => {
             el.addEventListener("click", () => {
@@ -268,6 +162,7 @@ class Settings {
     }
     closeModal() {
         if (this.currentModal) {
+            this.emit("modal.close", this.currentModal)
             this.modals[this.currentModal].classList.remove("active");
             this.modalScreen.classList.add("hidden");
         }
