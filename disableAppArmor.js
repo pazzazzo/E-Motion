4d923466f53798aa6b0a64b7eb8fcfe274ee956c
Lorenzo Azzopardi <54208876+pazzazzo@users.noreply.github.com>
2025-06-04 23:41:39 +0200

    Fix spelling in AppArmor disable message

--- disableAppArmor.js
+++ disableAppArmor.js
@@ -1,7 +1,7 @@
 const { exec } = require('child_process');
 
 function disableAppArmor() {
-  exec("gnome-terminal --maximize --title='Frist launch need root access' --zoom=2 -- sudo sysctl -w kernel.apparmor_restrict_unprivileged_userns=0", (error, stdout, stderr) => {
+  exec("gnome-terminal --maximize --title='First launch need root access' --zoom=2 -- sudo sysctl -w kernel.apparmor_restrict_unprivileged_userns=0", (error, stdout, stderr) => {
     if (error) {
       console.error(`Error executing command: ${error.message}`);
       return;
