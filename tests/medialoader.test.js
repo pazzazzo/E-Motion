4ec61c13de02c40126a713c4eff579ca8250bef3
pazzazzo <54208876+pazzazzo@users.noreply.github.com>
2025-06-08 18:16:49 +0200

    Update medialoader.test.js

--- tests/medialoader.test.js
+++ tests/medialoader.test.js
@@ -1,8 +1,2 @@
 const MediaLoader = require('../src/classes/MediaLoader');
-
-describe('MediaLoader Functions', () => {
-    const mediaLoader = new MediaLoader();
-    test("test", () => {
-        mediaLoader.preinit()
-    })
-});
+let mediaLoader;
